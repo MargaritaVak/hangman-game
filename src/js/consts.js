b5b93d946888d3231261a8a98babfa1845d7b265
MargaritaVak <vakhrusevarita2018@gmail.com>
2023-07-06 17:28:51 +0300

    Add game files

---
+++ src/js/consts.js
@@ -0,0 +1,5 @@
+export const WORDS = ['code', 'frontend', 'progamming','variable', 'function', 'class', 'object', 'array', 
+'string', 'integer', 'boolean', 'loop', 'conditional', 'algorithm', 'compiler', 'debugger', 
+'syntax', 'statement', 'parameter', 'argument', 'module', 'library', 'exception', 'file', 
+'input', 'output', 'binary', 'hexadecimal', 'recursion', 'interface', 'inheritance', 'polymorphism', 'encapsulation']
+export const KEYBOARD_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
